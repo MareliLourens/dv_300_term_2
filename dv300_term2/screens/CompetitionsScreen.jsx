6d2b7a8a15f2086930599a44428a82447eab5e6e
MareliLourens <221119@virtualwindow.co.za>
2024-05-29 21:38:54 +0200

    Read Categories

--- dv300_term2/screens/CompetitionsScreen.jsx
+++ dv300_term2/screens/CompetitionsScreen.jsx
@@ -1,14 +1,72 @@
-import React from 'react';
-import { View, Text, SafeAreaView } from 'react-native';
+import { Pressable, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { AntDesign } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
+import { getCategories } from '../services/DbService';
 
-const CompetitionsScreen = () => {
-  return (
-    <SafeAreaView>
-      <View style={{ padding: 20 }}>
-        <Text>Competitions</Text>
-      </View>
-    </SafeAreaView>
-  );
+const CompetitionsScreen = ({ navigation }) => {
+
+    const goToAdd = () => { navigation.navigate("Add") }
+
+    const [CategoryItems, setCategoryItems] = useState([])
+
+    useEffect(() => {
+        handleGettingOfData()
+        
+    }, [])
+
+    const handleGettingOfData = async () => {
+        var allData = await getCategories()
+        console.log("All data" + allData)
+        setCategoryItems(allData)
+    }
+    return (
+        <SafeAreaView>
+            <View style={styles.container}>
+
+        { CategoryItems.map((item, index) => (
+            <TouchableOpacity key={index} style={styles.card}>
+                    <Text>{item.name}</Text>
+                    <AntDesign name="star" size={24} color="orange" />
+                </TouchableOpacity>
+        )) }
+                
+            </View>
+
+        </SafeAreaView>
+    )
 }
 
-export default CompetitionsScreen;
+export default CompetitionsScreen
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 20
+    },
+    card: {
+        width: '100%',
+        backgroundColor: 'white',
+        padding: 15,
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginBottom: 20
+    },
+    addButton: {
+        backgroundColor: 'white',
+        borderColor: 'green',
+        borderWidth: 2,
+        padding: 10,
+        marginBottom: 20,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: 5
+    },
+    addButtonText: {
+        textAlign: 'center',
+        color: 'green',
+        fontWeight: 'bold'
+    }
+})
--- dv300_term2/services/DbService.js
+++ dv300_term2/services/DbService.js
@@ -1,21 +1,32 @@
 import { collection, addDoc } from "firebase/firestore";
 import { db } from "../firebase";
+import { getDocs } from "firebase/firestore";
 
 export const createNewEntry = async (Entry) => {
 
 
     try {
         const docRef = await addDoc(collection(db, "Entries"), Entry
-        // {
-        //     first: "Ada",
-        //     last: "Lovelace",
-        //     born: 1815
-        // }
-    );
+        );
         console.log("Document written with ID: ", docRef.id);
         return true
     } catch (e) {
         console.error("Error adding document: ", e);
         return false
     }
+}
+
+var allCategories = []
+
+export const getCategories = async () => {
+    const allCategories = []; // Declare allCategories inside the function
+
+    const querySnapshot = await getDocs(collection(db, "Categories"));
+    querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        // console.log(doc.id, " => ", doc.data());
+        allCategories.push({ ...doc.data(), id: doc.id });
+    });
+    console.log(allCategories);
+    return allCategories;
 }
